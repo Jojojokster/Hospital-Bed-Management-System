e0046fac4bfceb7d53527676026b257f649182f1
Joseph <josephajayiogunya@gmail.com>
2025-05-18 13:46:30 +0100

    -

--- config/database.js
+++ config/database.js
@@ -17,7 +17,6 @@ const sequelize = new Sequelize(
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ alter: true }); 
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
